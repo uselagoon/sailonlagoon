e4c4b0d15071d93222ee24dd1e3ec4612b4e8aac
Blaize Kaye <blaize.kaye@amazee.io>
2025-02-25 11:17:59 +1300

    Adds a service prioritization sort functionality to generating services

--- tests/Unit/GenTest.php
+++ tests/Unit/GenTest.php
@@ -10,9 +10,9 @@ it('will allow us to remove unused services', function () {
 
     $serviceList = collect(["cli", "php", "mariadb"]);
     $services = [
-      "first" => [
-          "depends_on" => ["cli", "php", "shouldntexist"]
-      ]
+        "first" => [
+            "depends_on" => ["cli", "php", "shouldntexist"]
+        ]
     ];
 
     $newServices = $sailonLagoonCommand->removeUnusedServiceDependencies($services, $serviceList);
@@ -21,14 +21,25 @@ it('will allow us to remove unused services', function () {
     expect($newServices["first"]["depends_on"])->toContain("cli");
 });
 
-it("should successfully generate a docker-compose.yaml given stubs", function() {
+it("should successfully generate a docker-compose.yaml given stubs", function () {
     $sailonLagoonCommand = new SailonlagoonCommand();
-    $serviceList = collect(["cli", "mariadb"]);
+    $serviceList = collect(["cli", "php", "nginx"]);
     $yamlFile = file_get_contents(join_paths(__DIR__, "assets/stubs", "docker-compose.stub"));
     $dockerCompose = $sailonLagoonCommand->generateDockerCompose($serviceList,
         join_paths(__DIR__, "assets/stubs"), $yamlFile);
 
     $yamlOutput = Yaml::dump($dockerCompose, 5);
+    dd($yamlOutput);
     expect($yamlOutput)->toContain("cli")->toContain("mariadb")->not()->toContain("php");
 
+});
+
+it("should order the services in the docker-compose.yaml in the correct order", function () {
+    $sailonLagoonCommand = new SailonlagoonCommand();
+    $serviceList = collect(["php", "cli", "nginx"]);
+    $yamlFile = file_get_contents(join_paths(__DIR__, "assets/stubs", "docker-compose.stub"));
+    $dockerCompose = $sailonLagoonCommand->generateDockerCompose($serviceList,
+        join_paths(__DIR__, "assets/stubs"), $yamlFile);
+
+    expect(array_keys($dockerCompose['services']))->toEqual(['cli', 'nginx', 'php']);
 });
